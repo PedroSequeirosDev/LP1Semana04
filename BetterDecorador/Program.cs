bda49580ecde0f1d7a3d13ab703c63f84674d514
PedroSequeirosDev <pedro.sequeiros1997@hotmail.com>
2025-03-12 20:24:17 +0000

    Added documentation to Main and Decor

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -2,8 +2,15 @@
 
 namespace BetterDecorador
 {
+    /// 
     public class Program
     {
+        /// <summary>
+        /// Main method which receives the arguments
+        /// uses the arguments in the Decor method
+        /// and converts the arguments to the desired format
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             char symbol = char.Parse(args[1]);
@@ -14,16 +21,26 @@ namespace BetterDecorador
 
         }
 
+        /// <summary>
+        /// Decor method, which receives a string, a char and an int
+        /// and returns an output with the desired formatting.
+        /// it also converts the char into a string and multiplies 
+        /// it by the value int
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="dec"></param>
+        /// <param name="i"></param>
+        /// <returns></returns>
         private static string Decor(string s, char dec, int i)
         {
-            string conv = Convert.ToString(dec);
+            string con = Convert.ToString(dec);
 
             for (int f = i - 1; f > 0; f--)
             {
-                conv += dec;
+                con += dec;
             }
 
-            return $"{conv} {s} {conv}";
+            return $"{con} {s} {con}";
         }
     }
 }
