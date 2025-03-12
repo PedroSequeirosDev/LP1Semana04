10f68db8b9566fca5b694df45735a3e0bd45e50b
PedroSequeirosDev <pedro.sequeiros1997@hotmail.com>
2025-03-12 19:05:54 +0000

    added file with int and char conversions

---
+++ BetterDecorador/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BetterDecorador
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            char symbol = char.Parse(args[1]);
+            int num = int.Parse(args[2]);
+
+            Decor(args[0], symbol, num);
+            Console.WriteLine(Decor(args[0], symbol, num));
+
+        }
+
+        private static string Decor(string s, char dec, int i)
+        {
+
+            return $"{i}*{dec} {s} {dec}*{i}";
+        }
+    }
+}
