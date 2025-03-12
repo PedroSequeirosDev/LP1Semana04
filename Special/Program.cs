4228e80e52bb7b4b567f6f9337aa97e2a6d8ade2
PedroSequeirosDev <pedro.sequeiros1997@hotmail.com>
2025-03-12 22:46:45 +0000

    Added recursive coding for special

--- Special/Program.cs
+++ Special/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Special
 {
@@ -6,7 +7,22 @@ namespace Special
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("Insert n-th number of the special sequence:");
+            string s = Console.ReadLine();
+
+            int num = int.Parse(s);
+            Console.WriteLine(GetSpecial(num));
         }
+        private static int GetSpecial(int n)
+        {
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+
+            return GetSpecial(n - 1) + GetSpecial(n - 2) * 2;
+
+        }
+
     }
 }
